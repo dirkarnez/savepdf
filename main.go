5ff50f21795fc5f6e488893b858b6935a4cfe2ac
dirkarnez <smalldirkalex@gmail.com>
2022-10-16 20:55:06 +0800

    - init, working

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
@@ -14,12 +15,19 @@ import (
 
 func main() {
 	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(
+		ctx,
+		chromedp.WithLogf(log.Printf))
 	defer cancel()
 
 	// capture pdf
 	var buf []byte
-	if err := chromedp.Run(ctx, printToPDF(`https://www.google.com/`, &buf)); err != nil {
+	if err := chromedp.Run(ctx, printToPDF(`https://mp.weixin.qq.com/s/Yf2pHXKwUjsLJ6J2wlnrOA`, &buf)); err != nil {
 		log.Fatal(err)
 	}
 
@@ -29,17 +37,41 @@ func main() {
 	fmt.Println("wrote sample.pdf")
 }
 
+// var body = document.body,
+//     html = document.documentElement;
+
+// var height = Math.max( body.scrollHeight, body.offsetHeight,
+//                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+
 // print a specific pdf page.
 func printToPDF(urlstr string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
+	tasks := chromedp.Tasks{}
+
+	tasks = append(tasks,
 		chromedp.Navigate(urlstr),
+		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
+		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
+		// chromedp.WaitVisible(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(6) > img")`, chromedp.ByJSPath),
+	)
+	// var nodes []*cdp.Node
+	tasks = append(tasks,
+		// chromedp.Nodes(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(5) > p:nth-child(5)")`, &nodes, chromedp.ByJSPath),
+		// chromedp.MouseClickNode(nodes[0], chromedp.ButtonType(input.Right)),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
+		// chromedp.WaitVisible(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(5) > p:nth-child(5) > img")`, chromedp.ByJSPath),
+		chromedp.Sleep(5*time.Second),
+		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
+		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			buf, _, err := page.PrintToPDF().WithPrintBackground(false).Do(ctx)
+			buf, _, err := page.PrintToPDF().WithPaperHeight(74.71875).WithPrintBackground(false).Do(ctx)
 			if err != nil {
 				return err
 			}
 			*res = buf
 			return nil
 		}),
-	}
+	)
+
+	return tasks
 }
