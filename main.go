d120d07474afd1d83c35ba328e8f4ac0c292d277
dirkarnez <smalldirkalex@gmail.com>
2022-10-23 22:31:42 +0800

    - beta

--- main.go
+++ main.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,11 +13,23 @@ import (
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/dslipak/pdf"
+)
+
+var (
+	url string
 )
 
 func main() {
-	// create context
+	flag.StringVar(&url, "url", "", "target url")
+	flag.Parse()
 
+	if len(url) < 1 {
+		log.Fatalln("--url is needed")
+		return
+	}
+
+	// create context
 	opts := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))
 	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
@@ -27,12 +41,14 @@ func main() {
 
 	// capture pdf
 	var buf []byte
-	if err := chromedp.Run(ctx, printToPDF(`https://mp.weixin.qq.com/s/Yf2pHXKwUjsLJ6J2wlnrOA`, &buf)); err != nil {
+	if err := chromedp.Run(ctx, printToPDF(url, &buf)); err != nil {
 		log.Fatal(err)
+		return
 	}
 
 	if err := ioutil.WriteFile("sample.pdf", buf, 0o644); err != nil {
 		log.Fatal(err)
+		return
 	}
 	fmt.Println("wrote sample.pdf")
 }
@@ -49,26 +65,58 @@ func printToPDF(urlstr string, res *[]byte) chromedp.Tasks {
 
 	tasks = append(tasks,
 		chromedp.Navigate(urlstr),
-		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
-		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
+		// chromedp.Sleep(1*time.Second),
 		// chromedp.WaitVisible(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(6) > img")`, chromedp.ByJSPath),
 	)
 	// var nodes []*cdp.Node
 	tasks = append(tasks,
 		// chromedp.Nodes(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(5) > p:nth-child(5)")`, &nodes, chromedp.ByJSPath),
 		// chromedp.MouseClickNode(nodes[0], chromedp.ButtonType(input.Right)),
+		chromedp.Sleep(5*time.Second),
+		chromedp.EvaluateAsDevTools(`Array.from(document.body.getElementsByTagName("*"))
+		.filter(element => element.style.overflow != "visible")
+		.forEach(element => {
+			element.style.overflow = "visible";
+		})`, nil),
+		chromedp.EvaluateAsDevTools(`Array.from(document.body.getElementsByTagName("pre"))
+		.forEach(element => {
+			element.style.whiteSpace = "pre-wrap";
+			element.style.wordBreak = "break-word";
+		})`, nil),
 		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
 		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
 		// chromedp.WaitVisible(`document.querySelector("#js_content > section:nth-child(53) > section:nth-child(5) > p:nth-child(5) > img")`, chromedp.ByJSPath),
-		chromedp.Sleep(5*time.Second),
-		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
-		chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
+		// chromedp.Sleep(5*time.Second),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(0, 0)", nil),
+		// chromedp.EvaluateAsDevTools("document.documentElement.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)", nil),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			buf, _, err := page.PrintToPDF().WithPaperHeight(74.71875).WithPrintBackground(false).Do(ctx)
-			if err != nil {
-				return err
+			var height = 100.0
+			var isMultiplePage = true
+
+			for isMultiplePage {
+				buf, _, err := page.PrintToPDF().WithPaperHeight(height).WithPrintBackground(true).Do(ctx)
+				if err != nil {
+					return err
+				}
+
+				reader := bytes.NewReader(buf)
+
+				pdfReader, err := pdf.NewReader(reader, int64(len(buf)))
+				if err != nil {
+					return err
+				}
+
+				numPage := pdfReader.NumPage()
+				if numPage == 1 {
+					isMultiplePage = false
+					*res = buf
+				} else {
+					height = height + 10
+				}
 			}
-			*res = buf
+
 			return nil
 		}),
 	)
